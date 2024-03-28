e67edf8462bb8cbdd9931076755bfba3ad7dcf47
mabo98 <felipemb15t@gmail.com>
2024-03-28 11:54:26 -0600

    Project-Structure

---
+++ src/test/java/org/grupo_e/rungroup/RungroupApplicationTests.java
@@ -0,0 +1,13 @@
+package org.grupo_e.rungroup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RungroupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
